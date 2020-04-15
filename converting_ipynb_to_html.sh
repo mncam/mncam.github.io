142c9682a4d41dd88f1980bfcc222645c1ffcef3
Mehmet Numan <mncam@192.168.1.53>
2020-04-16 00:04:07 +0300

    Initial commit

--- converting_ipynb_to_html.sh
+++ converting_ipynb_to_html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jupyter nbconvert --to html --ExecutePreprocessor.enabled=True index.ipynb
+jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True index.ipynb
 git add --all
 git commit -m "Initial commit"
 git push -u origin master
